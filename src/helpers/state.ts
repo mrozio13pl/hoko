a5c05c582224dbb4a72d91d1aee2afe214a973cf
mrozio13pl <41209892+mrozio13pl@users.noreply.github.com>
2025-01-08 18:15:52 +0100

    fix: `$.state` malfunctioning with multiple properties

--- src/helpers/state.ts
+++ src/helpers/state.ts
@@ -3,11 +3,18 @@ import { initState } from '@/shared';
 export function createState<T extends Record<string, any>>(
     value: T
 ): { [K in keyof T]: T[K] } {
-    return Object.entries(value).reduce(
-        (acc, [key, initialValue]) =>
-            initState(key, initialValue) as { [K in keyof T]: T[K] },
-        {} as { [K in keyof T]: T[K] }
-    );
+    return Object.entries(value).reduce((acc, [key, initialValue]) => {
+        const state = initState(key, initialValue);
+
+        Object.defineProperty(acc, key, {
+            get: () => state[key],
+            set: (newValue) => (state[key] = newValue),
+            enumerable: true,
+            configurable: true,
+        });
+
+        return acc;
+    }, {} as { [K in keyof T]: T[K] });
 }
 
 export { createState as state };
